0265662e7a5ed70099a415683742f9f6fc9bb535
Ebin Antony <ebinantonyhere@gmail.com>
2026-03-17 12:48:19 +0530

    updated theme , add conncet button

--- lib/features/discover/view/screens/discover_page.dart
+++ lib/features/discover/view/screens/discover_page.dart
@@ -12,32 +12,37 @@ class DiscoverPage extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text("Discover")),
-      body: state.when(
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (e, _) => Center(child: Text("Error: $e")),
-        data: (users) {
-          return GridView.builder(
-            padding: const EdgeInsets.all(12),
-            itemCount: users.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 12,
-              crossAxisSpacing: 12,
-              childAspectRatio: 0.75,
-            ),
-            itemBuilder: (context, index) {
-              final user = users[index];
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: state.when(
+            loading: () => const Center(child: CircularProgressIndicator()),
+            error: (e, _) => Center(child: Text("Error: $e")),
+            data: (users) {
+              return GridView.builder(
+                padding: const EdgeInsets.all(12),
+                itemCount: users.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 20,
+                  childAspectRatio: 0.75,
+                ),
+                itemBuilder: (context, index) {
+                  final user = users[index];
 
-              return DiscoverCard(
-                user: user,
-                onClose: () =>
-                    ref.read(discoverProvider.notifier).moveToBottom(index),
-                onConnect: () =>
-                    ref.read(discoverProvider.notifier).connect(user.id),
+                  return DiscoverCard(
+                    user: user,
+                    onClose: () =>
+                        ref.read(discoverProvider.notifier).moveToBottom(index),
+                    onConnect: () =>
+                        ref.read(discoverProvider.notifier).connect(user.id),
+                  );
+                },
               );
             },
-          );
-        },
+          ),
+        ),
       ),
     );
   }
--- lib/features/discover/view/widgets/dicovery_card.dart
+++ lib/features/discover/view/widgets/dicovery_card.dart
@@ -1,4 +1,7 @@
 import 'package:conet_app/features/discover/model/discover_user_model.dart';
+import 'package:conet_app/util/constant/colors.dart';
+import 'package:conet_app/util/constant/images.dart';
+import 'package:conet_app/util/helpers/helpers.dart';
 import 'package:flutter/material.dart';
 
 class DiscoverCard extends StatelessWidget {
@@ -15,11 +18,20 @@ class DiscoverCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dark = AppHelpers.isDarkMode(context);
+
     return Container(
-      padding: const EdgeInsets.all(12),
+      padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 20,
+            offset: Offset(0, 10),
+          ),
+        ],
         borderRadius: BorderRadius.circular(20),
-        color: Colors.grey.shade900,
+        color: dark ? AppColors.containerDark : AppColors.containerLight,
       ),
       child: Column(
         children: [
@@ -35,54 +47,109 @@ class DiscoverCard extends StatelessWidget {
           const SizedBox(height: 8),
 
           /// Avatar
-          CircleAvatar(
-            radius: 30,
-            backgroundImage: user.avatar.startsWith('http')
-                ? NetworkImage(user.avatar)
-                : AssetImage(user.avatar) as ImageProvider,
+          ///
+          Container(
+            width: 70,
+            height: 70,
+            decoration: BoxDecoration(
+              color: dark ? AppColors.containerDark : AppColors.containerLight,
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.grey),
+            ),
+            child: Image(
+              image: user.avatar.isNotEmpty
+                  ? AssetImage(user.avatar)
+                  : AssetImage(Appimages.defaultAvatar),
+            ),
           ),
 
-          const SizedBox(height: 8),
+          // ///
+          // CircleAvatar(
+          //   radius: 30,
+          //   backgroundImage: user.avatar.startsWith('http')
+          //       ? NetworkImage(user.avatar)
+          //       : AssetImage(user.avatar) as ImageProvider,
+          // ),
+          const SizedBox(height: 10),
 
           /// Name
-          Text(user.name, style: const TextStyle(fontWeight: FontWeight.bold)),
+          Text(
+            user.name,
+            style: Theme.of(
+              context,
+            ).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
+          ),
 
-          const SizedBox(height: 6),
+          const SizedBox(height: 10),
 
           /// Interest
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
             decoration: BoxDecoration(
-              color: Colors.pink,
-              borderRadius: BorderRadius.circular(20),
+              color: dark ? AppColors.skillChipdark : AppColors.skillChiplight,
+              borderRadius: BorderRadius.circular(30),
             ),
-            child: Text(user.interest, style: const TextStyle(fontSize: 12)),
+            child: Text(
+              user.interest,
+              style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                fontWeight: FontWeight.bold,
+                color: AppColors.skillLabel,
+              ),
+            ),
           ),
 
-          const SizedBox(height: 6),
+          const SizedBox(height: 10),
 
           /// Learning
-          Text(
-            "Wants: ${user.wantsToLearn}",
-            style: const TextStyle(fontSize: 12),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Want to Learn: ",
+                style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),
+              ),
+              Text(
+                user.wantsToLearn,
+                style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.learnLabel,
+                ),
+              ),
+            ],
           ),
-
           const Spacer(),
 
           /// Connect button
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: onConnect,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.pink,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              gradient: LinearGradient(
+                colors: [AppColors.gradientColor1, AppColors.gradientColor2],
+              ),
+            ),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: onConnect,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                child: Text(
+                  "Connect",
+                  style: Theme.of(
+                    context,
+                  ).textTheme.bodyLarge!.copyWith(color: Colors.white),
                 ),
               ),
-              child: const Text("Connect"),
             ),
           ),
+          SizedBox(height: 10),
         ],
       ),
     );
